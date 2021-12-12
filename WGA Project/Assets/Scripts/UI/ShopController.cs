6722b099185f56a0d965842777e7c428aded19fe
Blavez <76618921+Blavez@users.noreply.github.com>
2021-12-12 15:29:36 +0300

    сделан хил игрока за валюту

--- WGA Project/Assets/Scripts/UI/ShopController.cs
+++ WGA Project/Assets/Scripts/UI/ShopController.cs
@@ -6,24 +6,20 @@ using UnityEngine.UI;
 public class ShopController : MonoBehaviour
 {
     [SerializeField] private Button _buttonHealFrog;
-    [SerializeField] private Button _buttonHealRaft;
     [SerializeField] private int _healthGain;
 
     private void Awake()
     {
         _buttonHealFrog.onClick.AddListener(HealFrog);
-        _buttonHealRaft.onClick.AddListener(HealRaft);
-    }
-
-    private void HealRaft()
-    {
-        GameSettings.healtRaft += _healthGain;
-        Debug.Log(GameSettings.healtRaft);
     }
 
     private void HealFrog()
     {
-        GameSettings.healthFrog += _healthGain;
-        Debug.Log(GameSettings.healthFrog);
+        if (GameSettings.pollen>=2) 
+        {
+            GameSettings.pollen -= 2;
+            GameSettings.playerHP += _healthGain;
+            Debug.Log(GameSettings.playerHP);
+        }
     }
 }
