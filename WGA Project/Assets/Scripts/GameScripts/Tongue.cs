3ceddeedb66fcbd1a27d72fb5f2731c99dad3a19
taganin <Mumka@icloud.com>
2021-12-12 09:12:55 +0300

    Новая механика прицела и стрельбы

--- WGA Project/Assets/Scripts/GameScripts/Tongue.cs
+++ WGA Project/Assets/Scripts/GameScripts/Tongue.cs
@@ -4,55 +4,135 @@ using UnityEngine;
 
 public class Tongue : MonoBehaviour
 {
-    [SerializeField] float _maxAttackDistance;
-    [SerializeField] float tongueSpeed;
+    [SerializeField] Material tongueMaterial;
+    [SerializeField] Material aimMaterial;
+    [SerializeField] Transform frogMouth;
+    [SerializeField] float maxPower;
+    [SerializeField] float powerGrowSpeed;
     [SerializeField] float tongueDamage;
-    float attackDistance;
+    [SerializeField] float tongueSpeed;
+    [SerializeField] float tongueMinimalDistance;
+    LineRenderer tongueLine;
+    float currentPower;
+    Vector3 frogPosition;
+    Vector3 maxPowerPoint;
+    Vector3 attackVector;
 
+    private void Awake()
+    {
+        tongueLine = gameObject.GetComponent<LineRenderer>();
+        tongueLine.positionCount = 0;
+    }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && GameSettings.playerState != GameSettings.playerStates.attack)
+        frogPosition = frogMouth.position;
+        maxPowerPoint = frogPosition + (Vector3.Normalize(GameSettings.mousePosition - frogPosition) * maxPower);
+        if (Input.GetMouseButtonDown(0) && GameSettings.playerState == GameSettings.playerStates.raftControl)
         {
-            gameObject.transform.LookAt(GameSettings.mousePosition);
+            currentPower = 0;
+            GameSettings.playerState = GameSettings.playerStates.charging;
+        }
+        if (Input.GetMouseButtonUp(0) && GameSettings.playerState == GameSettings.playerStates.charging)
+        {
             GameSettings.playerState = GameSettings.playerStates.attack;
-            attackDistance = Vector3.Distance(gameObject.transform.position, GameSettings.mousePosition);
         }
 
-        if (GameSettings.playerState == GameSettings.playerStates.attack)
+        switch (GameSettings.playerState)
         {
-            gameObject.transform.Translate(Vector3.forward * Time.deltaTime * tongueSpeed);
-            float distance = Vector3.Distance(gameObject.transform.parent.transform.position, gameObject.transform.position);
-            if (distance > attackDistance || distance > _maxAttackDistance)
-            {
-                gameObject.transform.LookAt(gameObject.transform.parent.transform.position);
-            }
+            case GameSettings.playerStates.attack:
+                tongueLine.material = tongueMaterial;
+                Attack(frogPosition + attackVector);
+                break;
+            case GameSettings.playerStates.charging:
+                tongueLine.material = aimMaterial;
+                Charging();
+                break;
+            case GameSettings.playerStates.raftControl:
+                tongueLine.positionCount = 0;
+                break;
+            case GameSettings.playerStates.tongueReturn:
+                TongueReturn();
+                break;
+            default:
+                break;
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        switch (collision.gameObject.tag)
+        switch (other.gameObject.tag)
         {
             case "Player":
                 {
                     GameSettings.playerState = GameSettings.playerStates.raftControl;
-                    gameObject.transform.position = gameObject.transform.parent.transform.position;
+                    gameObject.transform.position = frogPosition;
                     break;
                 }
             case "Enemy":
                 {
-                    collision.gameObject.GetComponent<Enemy>().GetDamage(tongueDamage);
+                    other.gameObject.GetComponent<Enemy>().GetDamage(tongueDamage);
                     break;
                 }
             case "PickUp":
                 {
-                    collision.gameObject.GetComponent<PickUp>().GetHit();
+                    other.gameObject.GetComponent<PickUp>().GetHit();
                     break;
                 }
-            case null:
+            default:
                 {
                     break;
                 }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawSphere(maxPowerPoint, 0.5f);
+    }
+
+    void TongueReturn()
+    {
+        if (Vector3.Distance(frogPosition, gameObject.transform.position) <= tongueMinimalDistance)
+        {
+            gameObject.transform.position = frogPosition;
+            GameSettings.playerState = GameSettings.playerStates.raftControl;
+        }
+        gameObject.transform.LookAt(frogPosition);
+        gameObject.transform.Translate(Vector3.forward * Time.deltaTime * tongueSpeed);
+        DrawTongue();
+    }
+    void Charging()
+    {
+        currentPower = Mathf.Clamp(currentPower + (powerGrowSpeed * Time.deltaTime), 0, maxPower);
+        attackVector = GameSettings.mousePosition - frogPosition;
+        DrawAim();
+    }
+
+    void Attack(Vector3 target)
+    {
+        Debug.Log(currentPower);
+        if (Vector3.Distance(gameObject.transform.position, frogPosition) >= currentPower ||
+            Vector3.Distance(gameObject.transform.position, frogPosition) >= attackVector.magnitude)
+        {
+            GameSettings.playerState = GameSettings.playerStates.tongueReturn;
+        }
+        gameObject.transform.LookAt(target);
+        gameObject.transform.Translate(Vector3.forward * Time.deltaTime * tongueSpeed);
+        DrawTongue();
+    }
+
+    void DrawTongue()
+    {
+        tongueLine.positionCount = 2;
+        tongueLine.SetPosition(0, frogPosition);
+        tongueLine.SetPosition(1, gameObject.transform.position);
+    }
+
+    void DrawAim()
+    {
+        tongueLine.positionCount = 2;
+        tongueLine.SetPosition(0, frogPosition);
+        tongueLine.SetPosition(1, Vector3.Lerp(frogPosition, maxPowerPoint, currentPower/maxPower));
     }
 }
