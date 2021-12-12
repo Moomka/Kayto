da35d4f4cbee1ec2b421aeaa149c9dfd5447f1ed
Blavez <76618921+Blavez@users.noreply.github.com>
2021-12-12 17:50:47 +0300

    очень очень костыльная полоска хп

--- WGA Project/Assets/Scripts/UI/ShowHpPlayer.cs
+++ WGA Project/Assets/Scripts/UI/ShowHpPlayer.cs
@@ -1,45 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowHpPlayer : MonoBehaviour
-{
-    List<GameObject> sprites;
-    private void radfds()
-    {
-        float count;
-        count = GameSettings.playerHP / 100;
-        if (count*8<1)
-        {
-
-        }
-        else if(count * 8 < 2)
-        {
-
-        }
-        else if (count * 8 < 3)
-        {
-
-        }
-        else if (count * 8 < 4)
-        {
-
-        }
-        else if (count * 8 < 5)
-        {
-
-        }
-        else if (count * 8 < 6)
-        {
-
-        }
-        else if (count * 8 < 7)
-        {
-
-        }
-        else if (count * 8 < 8)
-        {
-
-        }
-    }
+{   
 }
