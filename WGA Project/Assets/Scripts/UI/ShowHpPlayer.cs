87a64665798a3b3291b6216cb4f3e201fbbb99fb
Blavez <76618921+Blavez@users.noreply.github.com>
2021-12-12 17:02:05 +0300

    хп игрока

---
+++ WGA Project/Assets/Scripts/UI/ShowHpPlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHpPlayer : MonoBehaviour
+{
+    List<GameObject> sprites;
+    private void radfds()
+    {
+        float count;
+        count = GameSettings.playerHP / 100;
+        if (count*8<1)
+        {
+
+        }
+        else if(count * 8 < 2)
+        {
+
+        }
+        else if (count * 8 < 3)
+        {
+
+        }
+        else if (count * 8 < 4)
+        {
+
+        }
+        else if (count * 8 < 5)
+        {
+
+        }
+        else if (count * 8 < 6)
+        {
+
+        }
+        else if (count * 8 < 7)
+        {
+
+        }
+        else if (count * 8 < 8)
+        {
+
+        }
+    }
+}
